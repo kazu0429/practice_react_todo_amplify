1c0a51bbcdf9e0a25361dde545e11558d6badb45
kazu0412 <75778286+kazu0429@users.noreply.github.com>
2023-08-16 16:50:38 +0900

    add todoForm

---
+++ src/views/components/AddTodo.tsx
@@ -0,0 +1,42 @@
+import { Box, Button, FormControl, FormErrorMessage, Input } from '@chakra-ui/react'
+import React from 'react'
+import { useForm } from 'react-hook-form'
+
+const AddTodo = () => {
+
+    const { handleSubmit, 
+            register, 
+            formState: { errors, isSubmitting }, 
+            reset 
+        } = useForm();
+    
+        const onSubmit = (data:{content:string}) => {
+            const {content} = data;
+            alert('追加完了')
+            reset()
+        }
+
+    return (
+        <Box display='flex' justifyContent='center'>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <FormControl 
+                    isInvalid={errors.content}
+                    w={{ base: '90vw', sm: '80vw', md: '70vw', lg: '60vw' }}>
+                    <Input 
+                        id='content' 
+                        placeholder='Enter todo' 
+                        {...register('content', {required:'Please enter todo.'})}
+                    />
+                    <FormErrorMessage>{errors.content && errors.content.message}</FormErrorMessage>
+                </FormControl>
+                <Box w='100%' display='flex' justifyContent='flex-end'>
+                    <Button mt={4} colorScheme='purple' isLoading={isSubmitting} type='submit'>
+                        Submit
+                    </Button>
+                </Box>
+            </form>
+        </Box>
+    )
+}
+
+export default AddTodo
