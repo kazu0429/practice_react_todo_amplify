2cf164f3ba16efabde0f26fc842f4811fca43204
kazu0412 <75778286+kazu0429@users.noreply.github.com>
2023-08-20 18:54:58 +0900

    add todoAPI

---
+++ src/stores/slices/todo/todoAPI.ts
@@ -0,0 +1,17 @@
+import { DataStore } from "aws-amplify";
+import { Todo } from "../../../models";
+
+export const createTodoApi = async(data:{content:string}) => {
+    const {content} = data;
+
+    try{
+        await DataStore.save(
+            new Todo({
+                content:content,
+                isDone:false,
+            })
+        )
+    }catch(err){
+        throw err;
+    }
+}
